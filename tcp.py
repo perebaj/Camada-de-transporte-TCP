51a84253e8e08163266e63bb646440a04cd7cb82
Jonathan <jonathan.silva@birdie.ai>
2021-10-05 23:40:26 -0300

    Teste 1 Ok

--- tcp.py
+++ tcp.py
@@ -18,23 +18,32 @@ class Servidor:
         self.callback = callback
 
     def _rdt_rcv(self, src_addr, dst_addr, segment):
-        src_port, dst_port, seq_no, ack_no, \
-            flags, window_size, checksum, urg_ptr = read_header(segment)
+        src_port, dst_port, seq_no, ack_no, flags, window_size, checksum, urg_ptr = read_header(
+            segment
+        )
 
         if dst_port != self.porta:
             # Ignora segmentos que não são destinados à porta do nosso servidor
             return
         if not self.rede.ignore_checksum and calc_checksum(segment, src_addr, dst_addr) != 0:
-            print('descartando segmento com checksum incorreto')
+            print("descartando segmento com checksum incorreto")
             return
 
-        payload = segment[4*(flags>>12):]
-        id_conexao = (src_addr, src_port, dst_addr, dst_port)
+        payload = segment[4 * (flags >> 12) :]
+        id_conexao = (src_addr, src_port, dst_addr, dst_port, seq_no, ack_no)
 
         if (flags & FLAGS_SYN) == FLAGS_SYN:
             # A flag SYN estar setada significa que é um cliente tentando estabelecer uma conexão nova
             # TODO: talvez você precise passar mais coisas para o construtor de conexão
-            conexao = self.conexoes[id_conexao] = Conexao(self, id_conexao)
+            conexao = self.conexoes[id_conexao] = Conexao(self, id_conexao, seq_no, ack_no)
+    
+
+            header = make_header(dst_port, src_port, seq_no, seq_no + 1, FLAGS_ACK + FLAGS_SYN)
+            header = fix_checksum(header, src_addr, dst_addr)
+            self.rede.enviar(header, src_addr)
+
+            self.conexao = Conexao(self, id_conexao, seq_no, ack_no)
+
             # TODO: você precisa fazer o handshake aceitando a conexão. Escolha se você acha melhor
             # fazer aqui mesmo ou dentro da classe Conexao.
             if self.callback:
@@ -43,27 +52,34 @@ class Servidor:
             # Passa para a conexão adequada se ela já estiver estabelecida
             self.conexoes[id_conexao]._rdt_rcv(seq_no, ack_no, flags, payload)
         else:
-            print('%s:%d -> %s:%d (pacote associado a conexão desconhecida)' %
-                  (src_addr, src_port, dst_addr, dst_port))
+            print(
+                "%s:%d -> %s:%d (pacote associado a conexão desconhecida)"
+                % (src_addr, src_port, dst_addr, dst_port)
+            )
 
 
 class Conexao:
-    def __init__(self, servidor, id_conexao):
+    def __init__(self, servidor, id_conexao, seq_no, ack_no):
         self.servidor = servidor
         self.id_conexao = id_conexao
         self.callback = None
-        self.timer = asyncio.get_event_loop().call_later(1, self._exemplo_timer)  # um timer pode ser criado assim; esta linha é só um exemplo e pode ser removida
-        #self.timer.cancel()   # é possível cancelar o timer chamando esse método; esta linha é só um exemplo e pode ser removida
+        self.seq_no = seq_no
+        self.ack_no = ack_no
+
+        self.timer = asyncio.get_event_loop().call_later(
+            1, self._exemplo_timer
+        )  # um timer pode ser criado assim; esta linha é só um exemplo e pode ser removida
+        # self.timer.cancel()   # é possível cancelar o timer chamando esse método; esta linha é só um exemplo e pode ser removida
 
     def _exemplo_timer(self):
         # Esta função é só um exemplo e pode ser removida
-        print('Este é um exemplo de como fazer um timer')
+        print("Este é um exemplo de como fazer um timer")
 
     def _rdt_rcv(self, seq_no, ack_no, flags, payload):
         # TODO: trate aqui o recebimento de segmentos provenientes da camada de rede.
         # Chame self.callback(self, dados) para passar dados para a camada de aplicação após
         # garantir que eles não sejam duplicados e que tenham sido recebidos em ordem.
-        print('recebido payload: %r' % payload)
+        print("recebido payload: %r" % payload)
 
     # Os métodos abaixo fazem parte da API
 
